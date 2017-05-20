97122e5066ade301571c9e8636e54d88cdb045c4
MaulBurz <maulana.muzakki@outlook.com>
2017-05-20 22:50:23 +0700

    Kode Utama (Loss: 0.38796)
    Menggunakan Logistic Regression dengan CountVectorizer yg ada di internet

--- xgboostPipeline-.py
+++ xgboostPipeline-.py
@@ -107,49 +107,6 @@ class cust_txt_col(BaseEstimator, TransformerMixin):
     def transform(self, df):
         return df[self.key].apply(str)
 
-# magic feature
-df1 = train_orig[['question1']].copy()
-df2 = train_orig[['question2']].copy()
-df1_test = test_orig[['question1']].copy()
-df2_test = test_orig[['question2']].copy()
-
-df2.rename(columns = {'question2':'question1'},inplace=True)
-df2_test.rename(columns = {'question2':'question1'},inplace=True)
-
-train_questions = df1.append(df2)
-train_questions = train_questions.append(df1_test)
-train_questions = train_questions.append(df2_test)
-train_questions.drop_duplicates(subset = ['question1'],inplace=True)
-
-train_questions.reset_index(inplace=True,drop=True)
-questions_dict = pd.Series(train_questions.index.values,index=train_questions.question1.values).to_dict()
-train_cp = train_orig.copy()
-test_cp = test_orig.copy()
-train_cp.drop(['qid1','qid2'],axis=1,inplace=True)
-
-test_cp['is_duplicate'] = -1
-test_cp.rename(columns={'test_id':'id'},inplace=True)
-comb = pd.concat([train_cp,test_cp])
-
-comb['q1_hash'] = comb['question1'].map(questions_dict)
-comb['q2_hash'] = comb['question2'].map(questions_dict)
-
-q1_vc = comb.q1_hash.value_counts().to_dict()
-q2_vc = comb.q2_hash.value_counts().to_dict()
-
-def try_apply_dict(x,dict_to_apply):
-    try:
-        return dict_to_apply[x]
-    except KeyError:
-        return 0
-#map to frequency space
-comb['q1_freq'] = comb['q1_hash'].map(lambda x: try_apply_dict(x,q1_vc) + try_apply_dict(x,q2_vc))
-comb['q2_freq'] = comb['q2_hash'].map(lambda x: try_apply_dict(x,q1_vc) + try_apply_dict(x,q2_vc))
-
-train_comb = comb[comb['is_duplicate'] >= 0][['id','q1_hash','q2_hash','q1_freq','q2_freq','is_duplicate']]
-test_comb = comb[comb['is_duplicate'] < 0][['id','q1_hash','q2_hash','q1_freq','q2_freq']]
-#		
-
 tfidf = TfidfVectorizer(max_df=1.0, min_df=1, ngram_range=(1, 1), max_features=100)
 reg = Pipeline([
         ('union', FeatureUnion(
@@ -167,7 +124,6 @@ reg = Pipeline([
                 ))])
 
 df = reg.fit_transform(train[['question1','question2']])
-
 x_train, x_valid, y_train, y_valid = train_test_split(df, train['is_duplicate'], test_size=0.2, random_state=0)
 
 params = {}
